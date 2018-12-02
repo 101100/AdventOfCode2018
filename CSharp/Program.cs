1ec77080b6ba3b94007cc5c58db0eb55cabd7fd0
Jason Heard <jasonpheard@gmail.com>
2018-12-02 10:47:00 -0700

    Add solution for Day 2

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -33,6 +33,15 @@ namespace AdventOfCode2018.CSharp
                 Console.WriteLine($"Part 2 (C#): {Program.Day1Part2(input)}");
                 Console.WriteLine($"Part 2 (F#): {Day1.part2(input)}");
             }
+            else if (day == 2)
+            {
+                var input = Inputs.GetInput(2);
+
+                Console.WriteLine($"Part 1 (C#): {Program.Day2Part1(input)}");
+                Console.WriteLine($"Part 1 (F#): {Day2.part1(input)}");
+                Console.WriteLine($"Part 2 (C#): {Program.Day2Part2(input)}");
+                Console.WriteLine($"Part 2 (F#): {Day2.part2(input)}");
+            }
             else
             {
                 Console.WriteLine($"I've never heard of day '{day}', sorry.");
@@ -77,6 +86,52 @@ namespace AdventOfCode2018.CSharp
             }
 
             return 0;
+        }
+
+
+        private static int Day2Part1(string input)
+        {
+            var counts = input
+                .SelectLines()
+                .Aggregate((TwoCount: 0, ThreeCount: 0), (oldCounts, nextString) =>
+                {
+                    var letterCounts = Program.Day2CountLetters(nextString);
+                    return (
+                        oldCounts.TwoCount + (letterCounts.Any(p => p.Value == 2) ? 1 : 0),
+                        oldCounts.ThreeCount + (letterCounts.Any(p => p.Value == 3) ? 1 : 0));
+                });
+
+            return counts.TwoCount * counts.ThreeCount;
+        }
+
+
+        private static ImmutableDictionary<char, int> Day2CountLetters(string input)
+        {
+            return input
+                .GroupBy(c => c)
+                .ToImmutableDictionary(g => g.Key, g => g.Count());
+        }
+
+
+        private static string Day2Part2(string input)
+        {
+            var different = input
+                .SelectLines()
+                .AllPairs()
+                .First(Program.Day2OneLetterDifferent);
+
+            return new string(different.Item1
+                .Zip(different.Item2, (left, right) => (Left: left, Right: right))
+                .Where(t => t.Left == t.Right)
+                .Select(t => t.Left)
+                .ToArray());
+        }
+
+        private static bool Day2OneLetterDifferent((string, string) inputPair)
+        {
+            return inputPair.Item1
+                .Zip(inputPair.Item2, (left, right) => (Left: left, Right: right))
+                .Count(t => t.Left != t.Right) == 1;
         }
 
     }
--- FSharp/Library.fs
+++ FSharp/Library.fs
@@ -5,6 +5,12 @@ module Seq =
     let repeat items =
         seq { while true do yield! items }
 
+    let allPairs items =
+        items
+        |> Seq.mapi (fun index1 item1 -> (Seq.mapi (fun index2 item2 -> if index1 = index2 then None else Some(item1, item2)) items))
+        |> Seq.concat
+        |> Seq.choose id
+
 
 module Util =
     let splitLinesSkipBlank (input: string) =
@@ -17,7 +23,7 @@ module Day1 =
     let frequencies input =
         input
         |> Util.splitLinesSkipBlank
-        |> Seq.map (string >> int)
+        |> Seq.map int
 
     let part1 input =
         input
@@ -30,4 +36,42 @@ module Day1 =
         |> Seq.repeat
         |> Seq.scan (+) 0
         |> Seq.scan (fun (set, repeat) next -> if Set.contains next set then set, Some(next) else (Set.add next set), None) (Set.empty, None)
-        |> Seq.pick (fun (set, repeat) -> repeat)
+        |> Seq.pick snd
+
+
+module Day2 =
+    let letterCounts line =
+        line
+        |> Seq.groupBy id
+        |> Seq.map (snd >> Seq.length)
+        |> Set.ofSeq
+
+    let part1 input =
+        input
+        |> Util.splitLinesSkipBlank
+        |> Seq.map letterCounts
+        |> Seq.fold
+            (fun (twos, threes) counts ->
+                twos + (if Set.contains 2 counts then 1 else 0),
+                threes  + (if Set.contains 3 counts then 1 else 0))
+            (0, 0)
+        |> fun (twos, threes) -> twos * threes
+
+    let oneLetterDifferent first second =
+        Seq.zip first second
+        |> Seq.filter ((<||) (<>))
+        |> Seq.length
+        |> (=) 1
+
+    let part2 input =
+        let pair =
+            input
+            |> Util.splitLinesSkipBlank
+            |> Seq.allPairs
+            |> Seq.find ((<||) oneLetterDifferent)
+
+        Seq.zip <|| pair
+        |> Seq.filter ((<||) (=))
+        |> Seq.map fst
+        |> Seq.map string
+        |> String.concat ""

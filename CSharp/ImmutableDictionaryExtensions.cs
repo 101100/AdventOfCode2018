5bb5f9ddd5c5ee086d6887e136781de4787d4151
Jason Heard <jasonpheard@gmail.com>
2019-11-29 21:09:43 -0700

    Add solution for Day 16

--- CSharp/ImmutableDictionaryExtensions.cs
+++ CSharp/ImmutableDictionaryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace AdventOfCode2018.CSharp
 {
@@ -14,6 +15,15 @@ namespace AdventOfCode2018.CSharp
             Func<TValue, TValue> updater)
         {
             return dictionary.SetItem(key, updater(dictionary.GetValueOrDefault(key, defaultInitialValue)));
+        }
+
+
+        public static string ToRegistersString<TValue>(
+            this ImmutableDictionary<int, TValue> dictionary)
+        {
+            return string.Join(",", dictionary
+                .OrderBy(p => p.Key)
+                .Select(p => $"{p.Key}:{p.Value}"));
         }
 
     }
